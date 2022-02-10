4d5fc26c71fb635036e066fa9fc41780d6bfdf86
isaacoliveira1 <isaac-nicholas@hotmail.com>
2022-02-10 19:29:33 -0300

    API REST NESTJS

---
+++ src/users/users.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User, UserDocument } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  create(createUserDto: CreateUserDto) {
+    const createUser = new this.userModel(createUserDto);
+    return createUser.save();
+  }
+
+  findAll() {
+    return this.userModel.find().exec();
+  }
+
+  findOne(id: string) {
+    return this.userModel.findById(id).exec();
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate
+    ({_id: id,}, 
+    {
+      $set: updateUserDto,
+    },
+    {
+      new: true
+    }
+    ).exec();
+  }
+
+  remove(id: string) {
+    return this.userModel.deleteOne({_id: id}).exec();
+  }
+
+
+}
